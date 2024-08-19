669b172807823673a91c2995beea68cf1d83b877
Scott Reynolds <scottreynolds6294@gmail.com>
2024-08-19 17:46:38 -0400

    connection/sequelize ammended to removie connect function

--- config/connections.js
+++ config/connections.js
@@ -11,12 +11,4 @@ const sequelize = new Sequelize(
    }
 );
 
-sequelize.connect(err => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-    } else {
-        console.log('Connected to the database');
-    }
-});
-
 module.exports = sequelize;
