9f11c95bd907f7776f58fd1f804aaa3b29bf8db9
Scott Reynolds <scottreynolds6294@gmail.com>
2024-08-19 14:39:37 -0400

    queries added

---
+++ db/queries.sql
@@ -0,0 +1,20 @@
+SELECT
+    role.id, 
+    role.title AS title, 
+    department.name AS department, 
+    role.salary
+FROM role
+JOIN department ON role.department_id = department.id;
+
+SELECT 
+    e.id, 
+    e.first_name, 
+    e.last_name, 
+    r.title AS title, 
+    d.name AS department, 
+    CONCAT(m.first_name, ' ', m.last_name) AS manager
+FROM employee e
+JOIN role r ON e.role_id = r.id
+JOIN department d ON r.department_id = d.id
+LEFT JOIN employee m ON e.manager_id = m.id;
+
--- db/schema.sql
+++ db/schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE department (
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL,
+    salary DECIMAL NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
@@ -21,7 +21,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_id INTEGER
+    manager_id INTEGER,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL
